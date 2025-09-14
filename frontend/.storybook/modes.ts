468cc40435c3251fe00b61cd5c74963473b6b605
Colin McCulloch <colin.mcculloch@whocanfixmycar.com>
2025-09-14 15:49:52 +0100

    chore: fix chromatic modes

--- frontend/.storybook/modes.ts
+++ frontend/.storybook/modes.ts
@@ -5,7 +5,13 @@ export const allModes = {
   dark: {
     theme: 'storypoint-shuffle (dark)',
   },
-  mobile: { name: 'Mobile', styles: { width: '425px', height: '844px' } },
-  tablet: { name: 'Tablet', styles: { width: '768px', height: '844px' } },
-  desktop: { name: 'Desktop', styles: { width: '1024px', height: '844px' } },
+  mobile: {
+    viewport: 'mobile',
+  },
+  tablet: {
+    viewport: 'tablet',
+  },
+  desktop: {
+    viewport: 'desktop',
+  },
 };
